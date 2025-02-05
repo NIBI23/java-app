6379bc17661ff3f752f54c256cf53372e6893afb
Nibi <Nibi@gmail.com>
2025-02-05 23:00:34 +0530

    src edit

--- src/main/java/com/devops/demo/HelloController.java
+++ src/main/java/com/devops/demo/HelloController.java
@@ -1,25 +1,17 @@
-package com.devops.demo;
+package com.minikube.sample;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MinikubeSampleApplicationTests {
 
-@WebMvcTest(HelloController.class)
-public class HelloControllerTest {
+	@Test
+	public void contextLoads() {
+	}
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Test
-    void testSayHello() throws Exception {
-        mockMvc.perform(get("/api/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("Hello, DevOps World!"));
-    }
 }
 
